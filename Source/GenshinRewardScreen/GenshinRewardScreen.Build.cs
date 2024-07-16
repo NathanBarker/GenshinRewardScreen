a601bfa688bc0349205090e3ed5739596bbfe24f
Nathan Barker <Nathanbarker142@gmail.com>
2024-07-16 19:41:51 +0100

    - Created new main screen widget blueprint, Pushed onto the viewport stack. Also enabled CommonUI and RiderLink for Unreal Engine. New Style guide for borders. New Level "NewWorld" is created.

--- Source/GenshinRewardScreen/GenshinRewardScreen.Build.cs
+++ Source/GenshinRewardScreen/GenshinRewardScreen.Build.cs
@@ -7,14 +7,15 @@ public class GenshinRewardScreen : ModuleRules
 	public GenshinRewardScreen(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PublicDependencyModuleNames.AddRange(new string[]
+			{ "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "CommonUI" });
 
 		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
---
+++ Source/GenshinRewardScreen/UI/MainScreen.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "MainScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHINREWARDSCREEN_API UMainScreen : public UCommonUserWidget
+{
+	GENERATED_BODY()
+};
