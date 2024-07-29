f65ab9c0ac93fc30a0937387fc0b51186f2d75f3
Nathan Barker <Nathanbarker142@gmail.com>
2024-07-29 21:25:02 +0100

    Polishing UI Assets and Widgets

---
+++ Source/GenshinRewardScreen/UI/Quests/QuestProgressionView.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "QuestProgressionView.h"
+
+void UQuestProgressionView::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+}
---
+++ Source/GenshinRewardScreen/UI/Quests/QuestProgressionView.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonUserWidget.h"
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "QuestProgressionView.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GENSHINREWARDSCREEN_API UQuestProgressionView : public UCommonUserWidget
+{
+	GENERATED_BODY()
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UProgressBar> ProgressBar = nullptr;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> QuestProgression = nullptr;
+
+	void NativePreConstruct() override;
+};
